49b12e0fbf3db77d5beb14a6cbfab91fcf4f6ebc
kaushik dey <109330283+kaushik-prasad-dey@users.noreply.github.com>
2023-10-16 19:47:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <title>::Low cost Website Design, Web Development and seo::</title>
 </head>
 <body>
-echo '<h1>This is Sample</h>';
+echo '<h1>This is Samplehhhhhh</h>';
 echo '<h2> changes in code...</h2>';
 </body>
 ?>
